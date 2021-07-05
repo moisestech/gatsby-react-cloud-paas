1202e895b66be50af085508641d0f11faba3cef8
moisestech <m@moises.tech>
2021-07-05 07:15:13 -0400

    init func comp 404 page

---
+++ gatsby/src/pages/404.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Nav from '../components/Nav';
+
+export default function FourOhFourPage() {
+  return (
+    <>
+      <p>Hey! - That page doesn't exist!!!</p>
+    </>
+  );
+}
