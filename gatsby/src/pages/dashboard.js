98b0210df3ee766f407ba2f3a2bea36a0612e6e7
moisestech <m@moises.tech>
2021-07-05 07:12:49 -0400

    init dashboard func comp

---
+++ gatsby/src/pages/dashboard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+export default function DashBoardPage() {
+  return (
+    <>
+      <p>Hey! - This is the Dashboard!!!</p>
+    </>
+  );
+}
