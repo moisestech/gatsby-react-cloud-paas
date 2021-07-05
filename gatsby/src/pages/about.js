2c38f912bada68435f75059194de3bc6e78c6153
moisestech <m@moises.tech>
2021-07-05 07:13:42 -0400

    init func comp about page

---
+++ gatsby/src/pages/about.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+export default function AboutPage() {
+  return (
+    <>
+      <p>Hey! - This is the About Page!!!</p>
+    </>
+  );
+}
