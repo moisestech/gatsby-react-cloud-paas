0a7e52c7c0dae93585600ff7db1a39347f179e00
moisestech <m@moises.tech>
2021-07-05 07:13:26 -0400

    init func comp account page

---
+++ gatsby/src/pages/account.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from '../components/Layout';
+import Nav from '../components/Nav';
+
+export default function AccountPage() {
+  return (
+    <>
+      <p>Hey! - This is the Account Page!!!</p>
+    </>
+  );
+}
