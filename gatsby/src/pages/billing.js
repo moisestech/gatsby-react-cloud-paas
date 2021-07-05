c3edd85efc21bf9b42908d55698fd7db395b3395
moisestech <m@moises.tech>
2021-07-05 07:13:11 -0400

    init func comp billing page

---
+++ gatsby/src/pages/billing.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+export default function BillingPage() {
+  return (
+    <Layout>
+      <p>Hey! - This is the Billing Page!!!</p>
+    </Layout>
+  );
+}
