e70d4043dbd59decab8f677211fdf478e76fabfd
moisestech <m@moises.tech>
2021-07-04 21:11:00 -0400

    init homePage, imp react, Layout

---
+++ gatsby/src/pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+export default function HomePage() {
+  return (
+    <>
+      <p>Hey! I'm a home page</p>
+    </>
+  );
+}
