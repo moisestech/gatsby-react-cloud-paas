e239e90e5cab61bbbc8be2733dd7ed7f477cbb0b
moisestech <m@moises.tech>
2021-07-06 13:10:47 -0400

    LayoutStyles GlobalStyles Typography Nav Footer

--- gatsby/src/components/Layout.js
+++ gatsby/src/components/Layout.js
@@ -1,14 +1,28 @@
 import React from 'react';
+import styled from 'styled-components';
+
 import Nav from './Nav';
 import Footer from './Footer';
+
 import 'normalize.css';
+import GlobalStyles from '../styles/GlobalStyles';
+import Typography from '../styles/Typography';
+
+const LayoutStyles = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+`;
 
 export default function Layout({ children }) {
   return (
-    <div>
+    <LayoutStyles>
+      <GlobalStyles />
+      <Typography />
+
       <Nav />
       {children}
       <Footer />
-    </div>
+    </LayoutStyles>
   );
 }
