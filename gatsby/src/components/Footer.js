29e27dedf46e51734cfaff70764282deeeef3bd2
moisestech <m@moises.tech>
2021-07-07 06:05:46 -0400

    Footer flex directions info description

--- gatsby/src/components/Footer.js
+++ gatsby/src/components/Footer.js
@@ -9,15 +9,27 @@ const FooterStyles = styled.footer`
   background: white;
   font-size: 1.2rem;
   background: salmon;
+  div {
+    display: flex;
+    flex: 1;
+    &:nth-child(2) {
+      justify-content: flex-end;
+    }
+  }
 `;
 
 export default function Footer() {
   return (
     <FooterStyles>
-      <p>Nevalyashka.com</p>
-      <p>Documentation</p>
-      <p>Blog</p>
-      <p>&copy; {new Date().getFullYear()} Nevalyashka</p>
+      <div>
+        <p>Nevalyashka.com</p>
+        <p>Documentation</p>
+        <p>Blog</p>
+      </div>
+
+      <div>
+        <p>&copy; {new Date().getFullYear()} Nevalyashka</p>
+      </div>
     </FooterStyles>
   );
 }
