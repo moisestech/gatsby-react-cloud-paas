d7cd29798ce0296937cc4f67f5eda661c49c01b6
moisestech <m@moises.tech>
2021-07-07 07:10:23 -0400

    init HomePageStyles

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
-import Layout from '../components/Layout';
+import styled from 'styled-components';
+
+const HomePageStyles = styled.div`
+  display: flex;
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  padding-left: 15px;
+  flex-direction: column;
+  margin-top: 75px;
+  z-index: 10;
+  p {
+    max-width: 400px;
+    font-size: 3rem;
+  }
+`;
 
 export default function HomePage() {
   return (
-    <>
-      <p>Hey! I'm a home page</p>
-    </>
+    <HomePageStyles>
+      <p>
+        Nevalyashka is a platform as a service (PaaS) that enables developers to
+        build, run, and operate applications entirely in the cloud.
+      </p>
+    </HomePageStyles>
   );
 }
