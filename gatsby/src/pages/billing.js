4644d3ff5957715804ad7643ce3c8301a609c6e3
moisestech <m@moises.tech>
2021-07-07 07:10:06 -0400

    init BillingPageStyles

--- gatsby/src/pages/billing.js
+++ gatsby/src/pages/billing.js
@@ -1,10 +1,25 @@
 import React from 'react';
-import Layout from '../components/Layout';
+import styled from 'styled-components';
+
+const BillingPageStyles = styled.div`
+  display: flex;
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  padding-left: 15px;
+  flex-direction: column;
+  margin-top: 75px;
+  z-index: 10;
+  p {
+    max-width: 400px;
+    font-size: 3rem;
+  }
+`;
 
 export default function BillingPage() {
   return (
-    <>
-      <p>Hey! - This is the Billing Page!!!</p>
-    </>
+    <BillingPageStyles>
+      <p>Billing Page 🧾</p>
+    </BillingPageStyles>
   );
 }
