537c4dbf7e30e76069bf7174ab73cb5ad46c05be
moisestech <m@moises.tech>
2021-07-07 07:09:47 -0400

    init AccountPageStyles

--- gatsby/src/pages/account.js
+++ gatsby/src/pages/account.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import Layout from '../components/Layout';
-import Nav from '../components/Nav';
+import styled from 'styled-components';
+
+const AccountPageStyles = styled.div`
+  display: flex;
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  padding-left: 15px;
+  flex-direction: column;
+  margin-top: 75px;
+  z-index: 10;
+  p {
+    max-width: 400px;
+    font-size: 3rem;
+  }
+`;
 
 export default function AccountPage() {
   return (
-    <>
-      <p>Hey! - This is the Account Page!!!</p>
-    </>
+    <AccountPageStyles>
+      <p>Account Page 👤</p>
+    </AccountPageStyles>
   );
 }
