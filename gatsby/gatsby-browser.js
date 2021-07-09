d94ed786f6d2503631d24be17316f9fbd1400712
moisestech <m@moises.tech>
2021-07-09 05:39:23 -0400

    testing ssr develop config for CSS consistency

--- gatsby/gatsby-browser.js
+++ gatsby/gatsby-browser.js
@@ -3,5 +3,6 @@ import Layout from './src/components/Layout';
 
 // https://www.gatsbyjs.com/docs/reference/config-files/gatsby-browser/#wrapPageElement
 export function wrapPageElement({ element, props }) {
+  // eslint-disable-next-line react/jsx-props-no-spreading
   return <Layout {...props}>{element}</Layout>;
 }
--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -1,1 +1,6 @@
 // This file is empty, but some people were reporting that it would not start unless they had an empty file. So here it is! You can delete the comment.
+module.exports = {
+  flags: {
+    DEV_SSR: true,
+  },
+};
