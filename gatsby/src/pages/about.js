c1045a4cb07aafbd48cb5bb688143bc11313bddc
moisestech <m@moises.tech>
2021-07-12 22:19:20 -0400

    simplify about page imports

--- gatsby/src/pages/about.js
+++ gatsby/src/pages/about.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
 
-export default function AboutPage() {
+const AboutPageStyles = styled.div`
+  display: flex;
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  padding-left: 15px;
+  flex-direction: column;
+  margin-top: 75px;
+  z-index: 10;
+  p {
+    max-width: 400px;
+    font-size: 3rem;
+  }
+`;
+
+export default function AccountPage() {
   return (
-    <>
-      <p>Hey! - This is the About Page!!!</p>
-    </>
+    <AboutPageStyles>
+      <p>About Page 👤</p>
+    </AboutPageStyles>
   );
 }
