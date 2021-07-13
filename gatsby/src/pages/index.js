1cb1203aaa4774f9a4ade979ab7e2d1d745cff2e
moisestech <m@moises.tech>
2021-07-12 22:14:22 -0400

    login isLoggedIn func ternary

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { Link } from 'gatsby';
 import styled from 'styled-components';
+import { isLoggedIn } from '../utils/auth';
 
 const HomePageStyles = styled.div`
   display: flex;
@@ -10,23 +12,36 @@ const HomePageStyles = styled.div`
   flex-direction: column;
   margin-top: 75px;
   z-index: 10;
-  p {
-    max-width: 450px;
-    font-size: 3rem;
-  }
   h2 {
     color: blue;
+  }
+  div {
+    max-width: 450px;
+    font-size: 3rem;
   }
 `;
 
 export default function HomePage() {
   return (
     <HomePageStyles>
-      <p>
+      <div>
         <h2>Nevalyashka</h2> is a platform as a service (PaaS) that enables
         developers to build, run, and operate applications entirely in the
         cloud.
-      </p>
+      </div>
+
+      <div>
+        {isLoggedIn() ? (
+          <>
+            You are logged in, so check your{' '}
+            <Link to="/app/profile">profile</Link>
+          </>
+        ) : (
+          <>
+            <Link to="/app/login">Log in</Link> to use our cloud services
+          </>
+        )}
+      </div>
     </HomePageStyles>
   );
 }
