5e275eaf1eae300262ab0687856da9c2f7653cbe
moisestech <m@moises.tech>
2021-07-13 06:27:59 -0400

    function syntax before name

--- gatsby/src/components/PrivateRoute.js
+++ gatsby/src/components/PrivateRoute.js
@@ -6,11 +6,15 @@ import { navigate } from 'gatsby';
 // AUTH
 import { isLoggedIn } from '../utils/auth';
 
-export default PrivateRoute({ component: Component, location, ...rest }) {
+export default function PrivateRoute({
+  component: Component,
+  location,
+  ...rest
+}) {
   if (!isLoggedIn() && location.pathname !== `/app/login`) {
     navigate('/app/login');
     return null;
   }
 
   return <Component {...rest} />;
-};
+}
