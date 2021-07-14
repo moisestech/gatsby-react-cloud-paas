4d7c05a1f742b5a220bb45a5aec178b9522942f8
moisestech <m@moises.tech>
2021-07-13 20:36:29 -0400

    LoginPageStyles

--- gatsby/src/pages/login.js
+++ gatsby/src/pages/login.js
@@ -12,6 +12,7 @@ import { setUser, isLoggedIn } from '../utils/auth';
 
 const LoginPageStyles = styled.div`
   display: flex;
+  width: 100%;
   flex: 1;
   align-items: center;
   justify-content: center;
@@ -19,7 +20,19 @@ const LoginPageStyles = styled.div`
   flex-direction: column;
   margin-top: 75px;
   z-index: 10;
-  background: red;
+  background-image: linear-gradient(to bottom, var(--blue), var(--pastelblue));
+  background-color: #534292;
+  .panel {
+    height: unset;
+    max-width: 430px;
+    margin: 0 auto 20px;
+    border-radius: 8px;
+    padding: 50px 0;
+    background-color: #fff;
+    border: 1px solid transparent;
+    -webkit-box-shadow: 0 1px 1px rgb(0 0 0 / 5%);
+    box-shadow: 0 1px 1px rgb(0 0 0 / 5%);
+  }
 `;
 
 export default function LoginPage() {
@@ -45,14 +58,16 @@ export default function LoginPage() {
   }
 
   return (
-    <LoginPageStyles title="Log In">
-      <p>Please sign-in to access to the private route:</p>
-      {firebase && (
-        <StyledFirebaseAuth
-          uiConfig={getUiConfig(firebase.auth)}
-          firebaseAuth={firebase.auth()}
-        />
-      )}
+    <LoginPageStyles>
+      <div className="panel">
+        <h2>Login in to your account</h2>
+        {firebase && (
+          <StyledFirebaseAuth
+            uiConfig={getUiConfig(firebase.auth)}
+            firebaseAuth={firebase.auth()}
+          />
+        )}
+      </div>
     </LoginPageStyles>
   );
 }
